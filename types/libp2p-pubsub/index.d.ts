06c7c8edd6bb633a059e58c4880c4fa513d72409
Tuyen Nguyen <vutuyen2636@gmail.com>
2020-07-01 05:47:14 +0700

    Types for libp2p 0.28

--- types/libp2p-pubsub/index.d.ts
+++ types/libp2p-pubsub/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for libp2p-pubsub 0.4.1
+// Type definitions for libp2p-pubsub 0.4.5
 // Project: https://github.com/libp2p/js-libp2p-pubsub
 /// <reference types="libp2p-gossipsub"/>
 
 declare interface LibP2pPubsubUtil {
-    normalizeInRpcMessage(msg: import("libp2p-gossipsub").IGossipMessage): import("libp2p-gossipsub").IGossipMessage;
+    normalizeInRpcMessage(msg: import("libp2p-gossipsub/src/message").Message): import("libp2p-gossipsub/src/message").Message;
 }
 
 declare module 'libp2p-pubsub' {
